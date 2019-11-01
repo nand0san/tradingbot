576d54dc5492dcdf6f5f05802cf50de2821f14a6
nand0san <hancaidolosdos@hotmail.com>
2019-11-01 22:13:21 +0100

    grafico calibrado con overlay

--- ta.py
+++ ta.py
@@ -8,60 +8,83 @@ from datetime import datetime
 from rsi_indicator import rsi
 
 
-def get_closes(candles):
-    closes = []
-    for candle in candles:
+def get_closes(_candles):
+    _closes = []
+    for candle in _candles:
         closestr = candle[4]
-        close = float(closestr)
-        closes.append(close)
-    return closes
+        _close = float(closestr)
+        _closes.append(_close)
+    return _closes
 
 
-def get_timestamp(candles):
-    timestamps = []
-    for candle in candles:
+def get_timestamp(_candles):
+    _timestamps = []
+    for candle in _candles:
         timestamp = candle[6]  # closing timestamp
         date = datetime.fromtimestamp(int(timestamp/1000))
-        timestamps.append(date)
-        # timestamps.append(timestamp)
-    return timestamps
+        _timestamps.append(date)
+        # _timestamps.append(timestamp)
+    return _timestamps
 
 
-def plot_graph(x, y, market):
-    plt.plot(x, y)
+def plot_graph(_x, _y, _market):
+    plt.plot(_x, _y)
     # plt.plot(x2, y2, label='Otra')
     plt.xlabel('Time')
     plt.ylabel('Price')
-    plt.title(market)
+    plt.title(_market)
     plt.rc('font', size=4)
     plt.show()
 
 
-def plot_rsi(x, y, market='TITULO', period=14):
-    x = x[period:]
-    plot_graph(x, y, market)
+def plot_rsi(_x, _y, _market='TITULO', period=14):
+    _x = _x[period:]
+    plt.axis([min(_x), max(_x), 0, 100])
+    plot_graph(_x, _y, _market)
 
-def plot_stacked(x, y1, y2, market='TITULO', period=14):
-    # x = x[period:]
-    # y2 = y2[period:]
-    fig, (ax1, ax2) = plt.subplots(2)
-    fig.suptitle(market)
-    ax1.plot(x, y1)
-    ax2.plot(x, y2)
+
+def plot_overlay(closes, rsi, timestamps):
+    closes = closes[14:]
+    timestamps = timestamps[14:]
+
+    fig, ax1 = plt.subplots()
+
+    color = 'tab:green'
+    ax1.set_xlabel('time')
+    ax1.set_ylabel('price', color=color)
+    ax1.set_ylim(min(closes), max(closes))
+    ax1.plot(timestamps, closes, color=color, linewidth=0.5)
+    ax1.tick_params(axis='y', labelcolor=color)
+
+    ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+
+    color = 'tab:blue'
+    ax2.set_ylabel('rsi', color=color)  # we already handled the x-label with ax1
+    ax2.set_ylim(-100, 200)
+    ax2.plot(timestamps, rsi, color=color,  linewidth=0.5)
+    ax2.tick_params(axis='y', labelcolor=color)
+
+    fig.tight_layout()  # otherwise the right y-label is slightly clipped
+
+    plt.show()
+
 
 # test
 market = 'BTCUSDT'
-interval = '1m'
+interval = '5m'
 rsi_interval = 14
 
 # get data
 candles = get_candles(market, interval)
 closes = get_closes(candles)
 timestamps = get_timestamp(candles)
+time = np.array(timestamps)
 
 # convert to numpy for rsi calculation
 prices = np.array(closes)
 rsi_result = rsi(prices, rsi_interval)
-# y = rsi_result.tolist()
-plot_rsi(timestamps, rsi_result)
-# plot_stacked(timestamps, closes, closes)
+y = rsi_result.tolist()
+
+# plot_rsi(timestamps, rsi_result)
+# plot_overlay(timestamps, closes, timestamps, y)
+plot_overlay(closes, rsi_result, time)
--- test.py
+++ test.py
@@ -3,12 +3,25 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-fig = plt.figure()
-ax1 = fig.add_axes([0.1, 0.5, 0.8, 0.4],
-                   xticklabels=[], ylim=(-1.2, 1.2))
-ax2 = fig.add_axes([0.1, 0.1, 0.8, 0.4],
-                   ylim=(-1.2, 1.2))
+# Create some mock data
+t = np.arange(0.01, 10.0, 0.01)
+data1 = np.exp(t)
+data2 = np.sin(2 * np.pi * t)
 
-x = np.linspace(0, 10)
-ax1.plot(np.sin(x))
-ax2.plot(np.cos(x));
+fig, ax1 = plt.subplots()
+
+color = 'tab:red'
+ax1.set_xlabel('time (s)')
+ax1.set_ylabel('exp', color=color)
+ax1.plot(t, data1, color=color)
+ax1.tick_params(axis='y', labelcolor=color)
+
+ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+
+color = 'tab:blue'
+ax2.set_ylabel('sin', color=color)  # we already handled the x-label with ax1
+ax2.plot(t, data2, color=color)
+ax2.tick_params(axis='y', labelcolor=color)
+
+fig.tight_layout()  # otherwise the right y-label is slightly clipped
+plt.show()
