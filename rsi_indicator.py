c754bf5b93a72cd5423321c8476850500103566a
nand0san <hancaidolosdos@hotmail.com>
2019-11-02 17:19:09 +0100

    ordenes firmadas correctamente en modo test

--- rsi_indicator.py
+++ rsi_indicator.py
@@ -5,6 +5,7 @@ import numpy as np
 def rsi(prices, period=14):
 
     # https://technical-indicators.readthedocs.io/en/latest/_modules/technical_indicators/technical_indicators.html
+    #
     # Input:
     #   prices ndarray
     #   period int > 1 and < len(prices) (optional and defaults to 14)
