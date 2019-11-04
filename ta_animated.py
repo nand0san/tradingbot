74de7fd416f83c2f8dbd71dc6e6a23e043a17627
nand0san <hancaidolosdos@hotmail.com>
2019-11-04 07:14:30 +0100

    errores de python

--- ta_animated.py
+++ ta_animated.py
@@ -8,10 +8,11 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
 
-def get_candles(market, tick_interval, limit=500):
+def getcandles(market, tick_interval, limit=500):
     # max limit 1000, default 500
     url = 'https://api.binance.com/api/v1/klines?symbol='+market+'&interval='+tick_interval+'&limit='+str(limit)
     data = requests.get(url).json()
+    print('Candles: getting...')
     return data
 
 def parse_timestamp_price_lists(candles):
@@ -20,34 +21,56 @@ def parse_timestamp_price_lists(candles):
         lines.append([candle[6], float(candle[4])])
     return lines
 
-def bufferFeed(comunicattor, buffer, market, interval):
+
+def bufferFeed(market, tick_interval, limit=50):
+    print(f'Iniciando buffer...')
+    # get initial data from binance
+    candles = getcandles(market, tick_interval, limit)
+    lines = parse_timestamp_price_lists(candles)
+    # create a initial buffer for closed candles
+    buffer = []
+    for line in lines:
+        current_time = time.time() * 1000
+        if current_time > line[0]:
+            buffer.append(line)
+    print(f'Buffer inicial: {buffer}')
     while True:
-        new_candles = get_candles(market, interval, 2)
-        new_line = parse_timestamp_price_lists(new_candles)
-        if new_line[0][0] != buffer[-1][0]:
-            print(f'Nueva vela cerrada: {new_line[0][0]}: {new_line[0][1]}')
-            buffer.append(new_line[0])
+        new_candles = getcandles(market, interval, 2)
+        new_lines = parse_timestamp_price_lists(new_candles)
+        if new_lines[0][0] != buffer[-1][0]:
+            print(f'Nueva vela cerrada: {new_lines[0][0]}: {new_lines[0][1]}')
+            buffer.append(new_lines[0])
             buffer.pop(0)
             # comunicattor.put([new_line[0][0], new_line[1][1]])  # ponemos solo los datos nuevos
             comunicattor.put(buffer)
         time.sleep(1)
 
-def update(frame, communicator: Queue): # here frame needs to be accepted by the function since this is used in FuncAnimations
-    data = communicator.get()  # this blocks untill it gets some data
+def animate(i): # here frame needs to be accepted by the function since this is used in FuncAnimations
+    data = comunicattor.get()  # this blocks untill it gets some data
+    xdata = []
+    ydata = []
     for line in data:
         xdata.append(line[0])
         ydata.append(line[1])
-        ln.set_data(xdata, ydata)
-    return ln,
+        # ln.set_data(xdata, ydata)
+    data = []
+    ax1.clear()
+    ax1.plot(xdata, ydata)
+
+    # return ln,
 
 
 if __name__ == '__main__':
 
     plt.style.use('seaborn')
-    fig, ax1 = plt.subplots()
 
-    xdata, ydata = [], []
-    ln, = plt.plot([], [])
+    fig = plt.figure()
+    ax1 = fig.add_subplot(1, 1, 1)
+
+    # # fig, ax1 = plt.subplots()
+    #
+    # xdata, ydata = [], []
+    # ln, = plt.plot([], [])
 
     comunicattor = Queue()
 
@@ -55,23 +78,23 @@ if __name__ == '__main__':
     market = 'BTCUSDT'
     interval = '1m'
 
-    # get initial data from binance
-    candles = get_candles(market, interval, 30)
-    lines = parse_timestamp_price_lists(candles)
+    # # get initial data from binance
+    # candles = getcandles(market, interval, 30)
+    # lines = parse_timestamp_price_lists(candles)
 
-    # create a initial buffer for closed candles
-    buffer = []
-    for line in lines:
-        current_time = time.time() * 1000
-        if current_time > line[0]:
-            buffer.append(line)
+    # # create a initial buffer for closed candles
+    # buffer = []
+    # for line in lines:
+    #     current_time = time.time() * 1000
+    #     if current_time > line[0]:
+    #         buffer.append(line)
 
-    # get closes and timestamps from buffer
-    for line in buffer:
-        timestamps = []
-        closes = []
-        timestamps.append(line[0])
-        closes.append(line[1])
+    # # get closes and timestamps from buffer
+    # for line in buffer:
+    #     timestamps = []
+    #     closes = []
+    #     timestamps.append(line[0])
+    #     closes.append(line[1])
 
     # first, plot old values in buffer
     # color = 'tab:green'
@@ -81,9 +104,9 @@ if __name__ == '__main__':
     # ax1.plot(timestamps, closes, color=color, linewidth=0.5)
     # ax1.tick_params(axis='y', labelcolor=color)
 
-    # start a process that get constant new closed candles to a queue
-    print('Start collecting from binance...')
-    p1 = Process(target=bufferFeed, args=(comunicattor, buffer, market, interval,))
+    # start a process that get constantly new closed candles to a queue
+    print('Started collecting from binance...')
+    p1 = Process(target=bufferFeed, args=(market, interval,))
     p1.start()
-    ani = FuncAnimation(fig, update, blit=True, fargs=(comunicattor,))
+    ani = FuncAnimation(fig, animate, blit=True, interval=1000)
     plt.show()
--- test6.py
+++ test6.py
@@ -1,25 +1,89 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
+import matplotlib.animation as animation
+from matplotlib import style
+from multiprocessing import Process, Queue
+import time
+import requests
 
-x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-y = [1, 12, 10, 1, 5, 3, 5, 7, 4, 4]
+style.use('fivethirtyeight')
 
-x_vals = []
-y_vals = []
+fig = plt.figure()
+ax1 = fig.add_subplot(1, 1, 1)
+xs = []
+ys = []
 
-cnt = 0
-def animate(i, cnt):
-    print(cnt)
-    print('loop ' + str(cnt))
 
-    x_vals.append(x[cnt])
-    y_vals.append(y[cnt])
+def get_candles(market, tick_interval, limit=500):
+    # max limit 1000, default 500
+    url = 'https://api.binance.com/api/v1/klines?symbol='+market+'&interval='+tick_interval+'&limit='+str(limit)
+    data = requests.get(url).json()
+    return data
 
-    print(x_vals, y_vals)
-    cnt += 1  # this is not happening
-    plt.plot(x_vals, y_vals, label='Price')
 
-ani = FuncAnimation(plt.gcf(), animate, fargs=(cnt,), interval=1000)
+def parse_timestamp_price_lists(candles):
+    lines = []
+    for candle in candles:
+        lines.append([candle[6], float(candle[4])])
+    return lines
 
-# plt.tight_layout()
+
+def animate(i):
+    x = []
+    y = []
+    data = com1.get()
+    print(f'Queue get: {data}')
+    for point in data:
+        x.append(point[0])
+        y.append(point[1])
+    data = []
+    ax1.clear()
+    ax1.plot(x, y)
+
+def read_data():
+
+    # init data
+    market = 'BTCUSDT'
+    tick_interval = '1m'
+
+    raw_data = get_candles(market, tick_interval, limit=50)
+    lines = parse_timestamp_price_lists(raw_data)
+    # create a initial buffer for closed candles
+    buffer = []
+    for line in lines:
+        current_time = time.time() * 1000
+        if current_time > line[0]:
+            buffer.append(line)
+    print(f'Buffer inicial: {buffer}')
+
+    while True:
+        new_candles = get_candles(market, tick_interval, 2)
+        new_lines = parse_timestamp_price_lists(new_candles)
+        if new_lines[0][0] != buffer[-1][0]:
+            print(f'Nueva vela cerrada: {new_lines[0][0]}: {new_lines[0][1]}')
+            buffer.append(new_lines[0])
+            buffer.pop(0)
+            # comunicattor.put([new_line[0][0], new_line[1][1]])  # ponemos solo los datos nuevos
+            com1.put(buffer)
+        time.sleep(1)
+
+com1 = Queue()
+
+# # test
+# market = 'BTCUSDT'
+# tick_interval = '1h'
+# data = get_candles(market, tick_interval)
+# print(data)
+# print(len(data))
+# print(type(data))
+#
+
+
+p1 = Process(target=read_data)
+p1.start()
+print('Animation starting...')
+ani = animation.FuncAnimation(fig, animate, interval=1000)
+
 plt.show()
