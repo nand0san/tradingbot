807d8e145dfe9571c158836d98895251f2e1995a
nand0san <hancaidolosdos@hotmail.com>
2019-11-13 22:49:18 +0100

    animacion conseguida

--- basicbot.py
+++ basicbot.py
@@ -1,17 +1,18 @@
 from multiprocessing import Process
-import numpy as np
+import pandas as pd
 import time
+from datetime import datetime
 import requests
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-import matplotlib.dates as mdates
+import matplotlib.dates as mpl_dates
 
 
-def getNparse_candles(market='BTCUSDT', tick_interval='1m', limit=10):  # devuelve dos arrays numpyc con precios y time
-    x = np.array([])  # time
-    y = np.array([])  # precio
+def getNparse_candles(market='BTCUSDT', tick_interval='1m', limit=10):
 
     while True:
+        result = []
+        time.sleep(3)
         try:
             # binance request
             url = 'https://api.binance.com/api/v1/klines?symbol='+market+'&interval='+tick_interval+'&limit='+str(limit)
@@ -20,55 +21,57 @@ def getNparse_candles(market='BTCUSDT', tick_interval='1m', limit=10):  # devuel
             for candle in candles:
                 close_price = float(candle[4])
                 close_time = candle[6]
-                if close_time < (int(round(time.time() * 1000))):  # solo velas cerradas
-                    x = np.append(x, close_time)
-                    y = np.append(y, close_price)
+
+                if close_time < (int(round(time.time()*1000))):  # solo velas cerradas
+                    my_date = datetime.fromtimestamp(close_time/1000)
+                    df = pd.DataFrame([[my_date, close_price]], columns=['Time', 'Price'])
+                    result.append(df)
+            result = pd.concat(result)
 
             # temp_file
-            np.savetxt('temp.txt', (x, y), delimiter=',')  # save to temp file
-            print(f'Ciclo Ultimo precio y time: {y[-1]} {x[-1]}')
-            time.sleep(3)
+            result.to_csv('temp.csv', index=False)
+            print(f'File updated {time.time()}')
+
+
         except KeyboardInterrupt:
             print('Keyboard interrupted')
-            return x, y  # devuelve dos numpy arrays
+            return
 
 def animate(i):
-    x, y = np.loadtxt('temp.txt', delimiter=',')
+    plt.style.use('seaborn')
+    data = pd.read_csv('temp.csv')
+    data['Time'] = pd.to_datetime(data['Time'])
+    data.sort_values('Time', inplace=True)
 
-    # xLimits = [min(x), max(x)]
-    # yLimits = [min(y), max(y)]
-    # lineS, = ax.plot([], [], lw=2)
-    # ax = plt.axes(xlim=xLimits, ylim=yLimits)
-    # lineS.set_data(x, y)
-
-    fig.autofmt_xdate()
-
-    ntimes = mdates.num2epoch(x / 1000.0)
+    price_date = data['Time']
+    price_close = data['Price']
 
     plt.cla()
 
-    ax1.plot(ntimes, y, 'r')
+    plt.plot_date(price_date, price_close, linestyle='solid')
 
-    # return lineS
+    plt.gcf().autofmt_xdate()
+
+    plt.title('Bitcoin Prices')
+    plt.xlabel('Date')
+    plt.ylabel('Closing Price')
+
+    plt.tight_layout()
 
 if __name__ == '__main__':
 
     market = 'BTCUSDT'
     tick_interval = '1m'
-    limit = 10
-
-    # candles = get_candles(market, tick_interval, 10)
-    # print(getNparse_candles())
+    limit = 100
 
     # proceso de actualziacion cosntante en marcha
     p1 = Process(target=getNparse_candles, args=(market, tick_interval, limit,))
     p1.start()
 
     # preparacion de ploteo
-    fig = plt.figure()
-    ax1 = fig.add_subplot(1, 1, 1)
-    ani = animation.FuncAnimation(fig, animate, interval=1000, blit=True)
+    # fig = plt.figure()
+
+    ani = animation.FuncAnimation(plt.gcf(), animate, interval=1000)
     plt.show()
 
-    # p1.join()
-
+    p1.join()
